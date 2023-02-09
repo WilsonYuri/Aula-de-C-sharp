71726911f8d11b742f778de67a05563ed77f189a
Wilson Yuri <wilsonyuri@icloud.com>
2023-02-09 19:58:27 -0300

    Renomear arquivos

---
+++ Aula 1 - Basico/Calculadora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+
+            string op;
+            int res = 0;
+
+            Console.WriteLine("Digite um número");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual operação? +, -, /, *");
+            op = Console.ReadLine();
+
+            //int soma = numero + numero;
+            //Console.WriteLine(soma);
+
+            if (op.Equals("+"))
+            {
+                res = numero1 + numero2;
+                Console.WriteLine("O Resultado é {0}", res);
+            }
+
+            else if (op.Equals("-"))
+            {
+                res = numero1 - numero2;
+                Console.WriteLine("O Resultado é {0}", res);
+            }
+
+            else if (op.Equals("/"))
+            {
+                res = numero1 / numero2;
+                Console.WriteLine("O Resultado é {0}", res);
+            }
+
+            else if (op.Equals("+"))
+            {
+                res = numero1 * numero2;
+                Console.WriteLine("O Resultado é {0}", res);
+            }
+
+            else
+            {
+                Console.WriteLine("Operação não existente");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
